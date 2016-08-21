38a7491dbffc2733a14ff5bed285cf55b963467b
jmsmuy <jmsmuy@gmail.com>
2016-08-20 22:34:27 -0300

    Fix pequeño en conexión con BE

--- FrontEnd/MyFirstApplication/app/src/main/java/com/example/andres/myfirstapplication/DesplayMessageActivity.java
+++ FrontEnd/MyFirstApplication/app/src/main/java/com/example/andres/myfirstapplication/DesplayMessageActivity.java
@@ -1,10 +1,14 @@
 package com.example.andres.myfirstapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.andres.myfirstapplication.ClienteHttp.UsuarioAPIClient;
 import com.example.andres.myfirstapplication.Models.Usuario;
@@ -18,6 +22,9 @@ import retrofit.Response;
 
 public class DesplayMessageActivity extends AppCompatActivity {
 
+    private Context context;
+    private UsuarioAPIClient.UsuarioEndpointInterface apiService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,29 +35,46 @@ public class DesplayMessageActivity extends AppCompatActivity {
         TextView textView = new TextView(this);
         textView.setTextSize(40);
 
+        this.context = this;
+
         //prueba call asyncronico
-        UsuarioAPIClient.UsuarioEndpointInterface apiService = UsuarioAPIClient.retrofit.create(UsuarioAPIClient.UsuarioEndpointInterface.class);
+        apiService = UsuarioAPIClient.retrofit.create(UsuarioAPIClient.UsuarioEndpointInterface.class);
 
-        String username = "Andres";
-        Call<Usuario> call = apiService.getUser(username);
+        new AsyncTaskPrototipo().execute();
 
-        call.enqueue(new Callback<Usuario>() {
-            @Override
-            public void onResponse(Response<Usuario> response) {
-                Usuario user = response.body();
-                String contraseña=user.getContraseña();
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-
-            }
-
-        });
 
         textView.setText(message);
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         layout.addView(textView);
+    }
+
+    private class AsyncTaskPrototipo extends AsyncTask<Void, Void, Void> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        protected Void doInBackground(Void... args) {
+            String username = "Andres";
+
+            Call<Usuario> call = apiService.getUser(username);
+
+            try {
+                Response<Usuario> response = call.execute();
+                Usuario user = response.body();
+                Log.i("INFO", user.getNombreUsuario() + " con clave : " + user.getContraseña());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Void empty) {
+            Toast.makeText(context, "Fin de lista alcanzado", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
