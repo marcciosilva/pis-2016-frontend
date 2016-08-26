66f988b1129dedcdf57507ad3df5f26d1048cc8b
Marccio Silva <marcciosilva@gmail.com>
2016-08-26 13:14:10 -0300

    Agregado prototipo usando realtime framework, sólo frontend (incluye tutorial en la aplicación, necesita internet)

---
+++ prototipo-realtime-framework/FrontendRealtimeFramework/app/src/androidTest/java/com/pis_2016/prototipos/tiempo_real/frontendrealtimeframework/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.pis_2016.prototipos.tiempo_real.frontendrealtimeframework;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
