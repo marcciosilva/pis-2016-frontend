768dd3b0e529a934d5ecf8f61629170d2da28566
Andres Veiro <andress_08@hotmail.com>
2016-09-05 19:43:36 -0300

    se agrego el servicio de multimedia y una aplicacion web para enviar archivos de prueba

---
+++ BackEnd/PruebaSubirImagenes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PruebaSubirImagenes.Startup))]
+namespace PruebaSubirImagenes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
