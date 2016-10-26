3e4246ad80efc14011d7f72ccc434c8bf440f973
Juan Manuel Serralta <juanserralta1@gmail.com>
2016-10-25 22:41:25 -0300

    Mejoras en el listado de extensiones de detalle de evento

--- EMSysMobile/app/src/main/java/com/sonda/emsysmobile/ui/views/adapters/EventDetailExtensionRecyclerViewAdapter.java
+++ EMSysMobile/app/src/main/java/com/sonda/emsysmobile/ui/views/adapters/EventDetailExtensionRecyclerViewAdapter.java
@@ -53,8 +53,9 @@ public class EventDetailExtensionRecyclerViewAdapter extends RecyclerView
         Log.d(TAG, "ZONE NAME: " + zoneString);
         holder.setItem(extension);
         holder.getIdAndZoneTextView().setText(zoneString);
-        holder.getDescriptionTextView().setText(extension.getDescription());
-        holder.getDateTextView().setText(DateUtils.dateToString(extension.getTimeStamp()));
+        if(extension.getDescription() != null && extension.getDescription() != "") {
+            holder.getDescriptionTextView().setText(extension.getDescription());
+        }
         holder.getView().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -76,8 +77,9 @@ public class EventDetailExtensionRecyclerViewAdapter extends RecyclerView
         private final View view;
         private final TextView idAndZoneTextView;
         private final TextView descriptionTextView;
-        private final TextView dateTextView;
+        private final TextView dispatcherTextView;
         private ExtensionDto item;
+
 
         public final View getView() {
             return view;
@@ -91,8 +93,8 @@ public class EventDetailExtensionRecyclerViewAdapter extends RecyclerView
             return descriptionTextView;
         }
 
-        public final TextView getDateTextView() {
-            return dateTextView;
+        public final TextView getDispatcherTextView() {
+            return descriptionTextView;
         }
 
         public final ExtensionDto getItem() {
@@ -108,7 +110,7 @@ public class EventDetailExtensionRecyclerViewAdapter extends RecyclerView
             this.view = view;
             idAndZoneTextView = (TextView) view.findViewById(R.id.label_id_and_zone);
             descriptionTextView = (TextView) view.findViewById(R.id.label_description);
-            dateTextView = (TextView) view.findViewById(R.id.label_date);
+            dispatcherTextView =  (TextView) view.findViewById(R.id.label_dispatcher);
         }
 
         @Override
