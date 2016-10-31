ac1d6f3e585d7828f861e7c8ca9d72775ddc8770
Juan Serra <juanpserra@hotmail.com>
2016-10-30 21:32:41 -0300

    Se agrega comunicacion con BE

---
+++ EMSysMobile/app/src/main/java/com/sonda/emsysmobile/backendcommunication/model/responses/ReportTimeResponse.java
@@ -0,0 +1,36 @@
+package com.sonda.emsysmobile.backendcommunication.model.responses;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Pape on 10/30/2016.
+ */
+
+public class ReportTimeResponse extends EmsysResponse{
+
+    @SerializedName("response")
+    private ErrorResponse innerResponse;
+
+    public final ErrorResponse getInnerResponse() {
+        return innerResponse;
+    }
+
+    public final void setInnerResponse(ErrorResponse innerResponse) {
+        this.innerResponse = innerResponse;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+
+        if (obj == null) {
+            return false;
+        }
+        if (!ReportTimeResponse.class.isAssignableFrom(obj.getClass())) {
+            return false;
+        }
+        final ReportTimeResponse other = (ReportTimeResponse) obj;
+        return ((super.equals(obj)) && (innerResponse.equals(other.innerResponse)));
+
+    }
+}
+
---
+++ EMSysMobile/app/src/main/java/com/sonda/emsysmobile/backendcommunication/services/request/ReportTimeRequest.java
@@ -0,0 +1,42 @@
+package com.sonda.emsysmobile.backendcommunication.services.request;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.lang.reflect.Type;
+
+/**
+ * Created by Pape on 10/30/2016.
+ */
+
+public class ReportTimeRequest<T> extends AbstractRequest {
+
+    public static final String REPORTTIME_PATH = "/eventos/reportarhoraarribo";
+    private static final String TAG = LoginRequest.class.getName();
+
+    private int idExtension;
+
+    public ReportTimeRequest(Context context, Type type, int idExtension) {
+        super(context, type, RequestType.POST);
+        this.idExtension = idExtension;
+    }
+
+    @Override
+    protected final String getPath() {
+        return REPORTTIME_PATH;
+    }
+
+    @Override
+    protected final JsonObject getBody() {
+        String json = new Gson().toJson(idExtension);
+        JsonObject jsonObject = (JsonObject) new JsonParser().parse(json);
+        Log.d(TAG, "Request body:");
+        Log.d(TAG, jsonObject.toString());
+        return jsonObject;
+    }
+
+}
